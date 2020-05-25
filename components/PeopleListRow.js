be561c7436dc7180b22d3f00267244981891dd78
ayushpaharia <ayush.paharia.18@gmail.com>
2020-05-25 17:23:14 +0530

    modal added - tasks-6/7

--- components/PeopleListRow.js
+++ components/PeopleListRow.js
@@ -1,6 +1,14 @@
-import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
-
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  TouchableHighlight,
+  Image,
+  Modal,
+} from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
 const s = StyleSheet.create({
   root: {
     padding: 10,
@@ -15,20 +23,17 @@ const s = StyleSheet.create({
 });
 
 const PeopleListRow = (data) => {
+  const [modalVisible, setModalVisible] = useState(false);
   const { title, first, last } = data.name;
   return (
-    <TouchableOpacity>
+    <TouchableOpacity
+      onPress={() => {
+        setModalVisible(true);
+      }}
+    >
       <View style={s.root}>
         <Image
-          style={{
-            width: 45,
-            height: 45,
-            borderWidth: 2,
-            borderColor: "#000",
-            borderRadius: 50,
-            backgroundColor: "red",
-            marginRight: 10,
-          }}
+          style={{ ...styles.image, width: 45, height: 45 }}
           source={{ uri: data.picture.medium }}
         />
         <Text
@@ -36,8 +41,166 @@ const PeopleListRow = (data) => {
           children={`${title}. ${first} ${last}`}
         />
       </View>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        // onRequestClose={() => {
+        //   Alert.alert("Modal has been closed.");
+        // }}
+      >
+        <View style={styles.centeredView}>
+          <View
+            style={{
+              ...styles.modalView,
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Image
+              style={{ ...styles.image, width: 80, height: 80 }}
+              source={{ uri: data.picture.medium }}
+            />
+            <View>
+              <Text
+                style={{
+                  ...styles.modalText,
+                  fontSize: 24,
+                  letterSpacing: -1,
+                  top: 12,
+                  fontWeight: "700",
+                }}
+                children={`${title}. ${first} ${last}`}
+              />
+              <Text
+                style={{
+                  ...styles.modalText,
+                  fontSize: 14,
+                }}
+                children={`${data.email}`}
+              />
+              <View
+                style={{
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                }}
+              >
+                <Text
+                  style={{
+                    ...styles.modalText,
+                    top: -8,
+                    fontSize: 10,
+                    fontWeight: "bold",
+                  }}
+                >
+                  phone
+                </Text>
+                <Text
+                  style={{
+                    ...styles.modalText,
+                    top: -8,
+                    fontSize: 10,
+                    fontWeight: "bold",
+                    left: -72,
+                  }}
+                >
+                  cell
+                </Text>
+              </View>
+              <View
+                style={{
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                  top: -20,
+                }}
+              >
+                <Text
+                  style={{
+                    ...styles.modalText,
+                    fontSize: 10,
+                    fontWeight: "bold",
+                  }}
+                  children={`${data.phone}`}
+                />
+                <Text
+                  style={{
+                    ...styles.modalText,
+                    left: -16,
+                    fontSize: 10,
+                    fontWeight: "bold",
+                  }}
+                  children={`${data.cell}`}
+                />
+              </View>
+            </View>
+
+            <TouchableOpacity
+              style={{
+                ...styles.openButton,
+                position: "absolute",
+                borderRadius: 50,
+                right: -10,
+                top: -10,
+              }}
+              onPress={() => {
+                setModalVisible(!modalVisible);
+              }}
+            >
+              <Icon name="times-circle" size={30} color="#fff" />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
     </TouchableOpacity>
   );
 };
+
+const styles = StyleSheet.create({
+  image: {
+    width: 45,
+    height: 45,
+    borderWidth: 2,
+    borderColor: "#000",
+    borderRadius: 50,
+    marginRight: 10,
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    paddingVertical: 35,
+    paddingHorizontal: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  openButton: {
+    backgroundColor: "#000",
+    paddingVertical: 6,
+    paddingHorizontal: 8,
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  modalText: {
+    marginBottom: 15,
+  },
+});
 
 export default PeopleListRow;
--- readme.md
+++ readme.md
@@ -18,7 +18,7 @@
 
 - [x] 4.  Implement live search functionality. (it should search without case sensitivity in the following properties: person's full name, email, phone & cell numbers)
 
-- [ ] 5.  Open detail modal - when clicking on a person, a modal should be opened displaying the following informations: full name, email, phone, cell phone, picture.
+- [x] 5.  Open detail modal - when clicking on a person, a modal should be opened displaying the following informations: full name, email, phone, cell phone, picture.
 
 - [ ] 6.  Fix SectionList's headers. People dictionary needs to be wrapped by the first character of their last name.
 
